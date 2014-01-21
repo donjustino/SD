cd9f3551f28abf14303b6ee85affabfba052c7f1
Justin <Justin@JUSTIN-VAIO>
2014-01-21 14:13:25 +0100

    COmmit SD

---
+++ SD/src/fr/unice/platdujour/application/Main.java
@@ -0,0 +1,185 @@
+package fr.unice.platdujour.application;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import fr.unice.platdujour.chord.Identifier;
+import fr.unice.platdujour.chord.Peer;
+import fr.unice.platdujour.chord.PeerImpl;
+import fr.unice.platdujour.chord.Tracker;
+import fr.unice.platdujour.chord.TrackerImpl;
+import fr.unice.platdujour.exceptions.AlreadyRegisteredException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * This class defines a main in which
+ * 1) a tracker is created
+ * 2) a Chord network is initialized
+ * 3) all the peers in the network are listed
+ * 4) a {@link GuideMichelin} is created. It will use the Chord network
+ * 5) some data are added to the {@link GuideMichelin}
+ * 6) the peers are listed again with the data they store
+ * 7) some data are requested from the {@link GuideMichelin}
+ */
+public class Main {
+	
+	/** Number of peers that will be injected in the network */
+	private static final int NB_PEERS = 10; 
+	
+	/** Port number of RMI registry */
+	private static final int RMI_REGISTRY_PORT = 1099;
+
+	/**
+	 * @param args Not used
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+
+		// A tracker is created
+		new TrackerImpl(RMI_REGISTRY_PORT);
+
+		Tracker tracker =
+				(Tracker) Naming.lookup("rmi://localhost:" + RMI_REGISTRY_PORT
+						+ "/tracker");
+		
+		// A Chord network is initialized
+		createNetwork(tracker);
+
+		// All the peers in the network are listed
+		Thread.sleep((long) (Math.log(NB_PEERS)*1000));
+		System.out.println("\nTurn around after first stabilization");
+		turnAround(tracker.getRandomPeer());
+
+		Thread.sleep((long) (Math.log(NB_PEERS)*1000));
+		System.out.println("\nTurn around after second stabilization");
+		turnAround(tracker.getRandomPeer());
+
+		// A GuideMichelin is created. It will use the Chord network
+		GuideMichelin guideMichelin = new GuideMichelinImpl(tracker);
+
+		// Some data are added to the {@link GuideMichelin}
+		DataGenerator dataGenerator = new DataGenerator(10);
+		Map<String, String> newData;
+
+		for (int i = 0 ; i < 10 ; i++) {
+			newData = dataGenerator.getNewData();      
+			for (Entry<String, String> entry : newData.entrySet()) {
+				guideMichelin.put(entry.getKey(), entry.getValue());
+			}
+		}
+
+		// The peers are listed again with the data they store
+		Thread.sleep(2000);
+		System.out.println("\nTurn around after adding data");
+		turnAround(tracker.getRandomPeer());
+		
+		// Some data are requested from the GuideMichelin
+		String[] restaurants = {"Le Bistrot Gourmand", "Auberge de la Madone", "toto"};
+
+		for (String restaurant : restaurants) {
+			System.out.println("\nRestaurant '" + restaurant + "' - Daily special: '"
+					+ guideMichelin.get(restaurant) + "'");
+		}
+                setsuccesorofsucessor(tracker.getRandomPeer());
+                serialization(tracker.getRandomPeer());
+                //deserialization(tracker.getRandomPeer());
+                killPeer(tracker.getRandomPeer(),tracker); 
+                int i = 0;
+                while (i == 0){
+                    listePeer(tracker.getRandomPeer());
+                }
+        }
+
+	/**
+	 * Creates a network composed of NB_PEERS peers.
+	 * @param tracker The tracker that is going to keep track of the peers
+	 * @throws RemoteException
+	 * @throws AlreadyRegisteredException If a peer tries to register more 
+	 * than once
+	 */
+	private static void createNetwork(Tracker tracker) 
+			throws RemoteException, AlreadyRegisteredException {
+		for (int i = 0 ; i < NB_PEERS ; i++) {
+			Peer p = new PeerImpl(new Identifier(i * 100));
+
+			if (i == 0) {
+				System.out.println("Ring created by " + p.getId());
+				p.create();
+			} 
+			else {
+				// The new peer is inserted in the network using a random peer 
+				// that already belongs to the network. This random peer is 
+				// retrieved thanks to the tracker.
+				Peer randomPeer = tracker.getRandomPeer();
+				System.out.println("Added " + p.getId() + " from "
+						+ randomPeer.getId() + " that points to "
+						+ randomPeer.getSuccessor().getId());
+				p.join(randomPeer);
+			}
+
+			tracker.register(p);
+		}
+	}
+
+	/**
+	 * This method run through the entire Chord network and print each 
+	 * encountered peer.
+	 * @param landmarkPeer The peer from which the turn starts
+	 * @throws RemoteException
+	 */
+	private static void turnAround(Peer landmarkPeer) throws RemoteException {
+		System.out.println(
+				"\nStarted turn around from " + landmarkPeer.getId());
+		Peer nextPeer = landmarkPeer;
+
+		do {
+			nextPeer = nextPeer.getSuccessor();
+			System.out.println("Visited " + nextPeer.describe());
+
+		} while (!nextPeer.equals(landmarkPeer));
+	}
+         private static void setsuccesorofsucessor(Peer landmarkPeer) throws RemoteException{
+            Peer nextPeer = landmarkPeer;
+            do {
+                 
+                           nextPeer = nextPeer.getSuccessor();
+                           nextPeer.setSuccesorofSuccessor(nextPeer.getSuccessor().getSuccessor());
+                          
+               } while ((!nextPeer.equals(landmarkPeer)));
+        }
+         private static void killPeer(Peer landmarkPeer, Tracker tracker) throws RemoteException, AlreadyRegisteredException{
+            Peer nextPeer = landmarkPeer;
+            boolean verif = false;
+             Identifier id = new Identifier(600);
+            do {
+                       nextPeer = nextPeer.getSuccessor();
+                    if(nextPeer.getId().equals(id)){
+                        System.out.println(nextPeer.getId());
+                        nextPeer.die();
+                        //tracker.delPeer(nextPeer);
+                        System.out.println("Mort");
+                        
+                        verif = true;
+               }               
+               } while ((verif != true));
+            
+        }
+          private static void listePeer(Peer landmarkPeer) throws RemoteException{
+            Peer nextPeer = landmarkPeer;
+            do {
+                        nextPeer = nextPeer.getSuccessor();
+                        System.out.println(nextPeer.describe());
+               } while ((!nextPeer.equals(landmarkPeer)));
+        }
+           private static void serialization(Peer landmarkPeer) throws RemoteException, IOException, FileNotFoundException, ClassNotFoundException{
+            Peer nextPeer = landmarkPeer;
+            do {
+                        nextPeer = nextPeer.getSuccessor();
+                        nextPeer.serialization(nextPeer.getSuccessor());
+                        nextPeer.deserialization();
+               } while ((!nextPeer.equals(landmarkPeer)));
+        }
+}
---
+++ SD/src/fr/unice/platdujour/chord/Tracker.java
@@ -0,0 +1,34 @@
+package fr.unice.platdujour.chord;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import fr.unice.platdujour.exceptions.AlreadyRegisteredException;
+import java.util.List;
+
+/**
+ * This interface defines the API of a tracker. It is supposed to keep track 
+ * of peers that joined and leaved the network.
+ */
+public interface Tracker extends Remote {
+
+	/**
+	 * Register a new peer in the structure maintained by the tracker.
+	 * @param peer The peer that has just joined the network
+	 * @throws AlreadyRegisteredException If the specified peer is already 
+	 * registered
+	 * @throws RemoteException
+	 */
+    void register(Peer peer) 
+    		throws AlreadyRegisteredException, RemoteException;
+
+    /**
+     * @return A peer that belongs to the network, randomly
+     * @throws RemoteException
+     */
+    Peer getRandomPeer() throws RemoteException;
+public List<Peer> getPeers() throws RemoteException ;
+        
+    public void setPeers(List<Peer> p)throws RemoteException ;
+    
+    public void delPeer(Peer peer) throws RemoteException, AlreadyRegisteredException;
+}
